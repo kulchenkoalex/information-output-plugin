e1033926921f7579bd7e149e32019c7808328042
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-02-02 10:40:59 +0200

    Standard information about the plugin

---
+++ information-output-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Information Output Plugin
+Plugin URI: https://github.com/kulchenkoalex/information-output-plugin
+Description: display information on the screen
+Version: 1.0
+Author: Alexey Kulchenko
+Author URI: http://wp8poltava.it-dev-lab.com/wp-site/
+*/
+?>
