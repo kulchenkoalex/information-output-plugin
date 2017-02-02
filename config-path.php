63cb6948471b2b272fa3a15b8a508def31b15b7d
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-02-02 14:03:00 +0200

    Setting constants

--- config-path.php
+++ config-path.php
@@ -1,3 +1,17 @@
 <?php
-
++define("INFORMATIONOUTPUT_PlUGIN_DIR", plugin_dir_path(__FILE__));
+ +define("INFORMATIONOUTPUT_PlUGIN_URL", plugin_dir_url( __FILE__ ));
+ +define("INFORMATIONOUTPUT_PlUGIN_SLUG", preg_replace( '/[^\da-zA-Z]/i', '_',  basename(STEPBYSTEP_PlUGIN_DIR)));
+ +define("INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN", str_replace( '_', '-', STEPBYSTEP_PlUGIN_SLUG ));
+ +define("INFORMATIONOUTPUT_PlUGIN_OPTION_VERSION", STEPBYSTEP_PlUGIN_SLUG.'_version');
+ +define("INFORMATIONOUTPUT_PlUGIN_OPTION_NAME", STEPBYSTEP_PlUGIN_SLUG.'_options');
+ +define("INFORMATIONOUTPUT_PlUGIN_AJAX_URL", admin_url('admin-ajax.php'));
+ +
+ +if ( ! function_exists( 'get_plugins' ) ) {
+ +    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+ +}
+ +$TPOPlUGINs = get_plugin_data(INFORMATIONOUTPUT_PlUGIN_DIR.'/'.basename(INFORMATIONOUTPUT_PlUGIN_DIR).'.php', false, false);
+ +
+ +define("INFORMATIONOUTPUT_PlUGIN_VERSION", $TPOPlUGINs['Version']);
+ +define("INFORMATIONOUTPUT_PlUGIN_NAME", $TPOPlUGINs['Name']);
 ?>
