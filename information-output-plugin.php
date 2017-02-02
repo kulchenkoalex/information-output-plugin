9de051ce833487af3e4308deb5265d2c6cda17cf
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-02-02 11:19:26 +0200

    License

--- information-output-plugin.php
+++ information-output-plugin.php
@@ -6,5 +6,18 @@ Description: display information on the screen
 Version: 1.0
 Author: Alexey Kulchenko
 Author URI: http://wp8poltava.it-dev-lab.com/wp-site/
+
+Copyright 2017  Kulchenko Alexey  (email: kulchenko.alexey@gmail.com)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 ?>
