c085d370c876b7211b9af86cca9101000eefab8f
kulchenkoalex <alexej07@bk.ru>
2017-02-09 00:03:14 +0200

    Create a class InformationOutputMyPluginsMenuController

---
+++ includes/controllers/admin/menu/InformationOutputMyPluginsMenuController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 09.02.2017
+ * Time: 0:02
+ */
+
+namespace includes\controllers\admin\menu;
+
+
+class InformationOutputMyPluginsMenuController extends InformationOutputBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_plugins_page(
+            __('Sub posts Information Output', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN),
+            __('Sub posts Information Output', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN),
+            'read',
+            'information_output_control_sub_posts_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page Plugins", INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
