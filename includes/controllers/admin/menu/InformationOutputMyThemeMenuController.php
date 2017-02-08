f87a652fcf12657a02f4fc7173f0819eb2887792
kulchenkoalex <alexej07@bk.ru>
2017-02-08 23:59:35 +0200

    Create a class InformationOutputMyThemeMenuController

---
+++ includes/controllers/admin/menu/InformationOutputMyThemeMenuController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 08.02.2017
+ * Time: 23:58
+ */
+
+namespace includes\controllers\admin\menu;
+
+
+class InformationOutputMyThemeMenuController extends InformationOutputBaseAdminMenuController
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+
+        $pluginPage = add_theme_page(
+            __('Sub posts Information Output', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN),
+            __('Sub posts Information Output', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN),
+            'read',
+            'information_output_control_sub_posts_menu',
+            array(&$this, 'render')
+        );
+    }
+
+    public function render()
+    {
+        // TODO: Implement render() method.
+        _e("Hello this page Theme", INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN);
+    }
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+        $instance = new self;
+        return $instance;
+    }
+}
