ff407247b79555a6ca4b2d92d2831e8f34b512cf
kulchenkoalex <alexej07@bk.ru>
2017-02-14 00:39:52 +0200

    Caching Access Token

--- includes/common/InformationOutputRequestApi.php
+++ includes/common/InformationOutputRequestApi.php
@@ -33,7 +33,7 @@ class InformationOutputRequestApi
             echo '</ul>';
         }
     }
-    
+
     public function generationToken()
     {
         $key = base64_encode( urlencode( "n8KP16uvGZA6xvFTtb8IAA:i4pmOV0duXJv7TyF5IvyFdh5wDIqfJOovKjs92ei878" ) );
@@ -47,5 +47,19 @@ class InformationOutputRequestApi
         ));
         $token = json_decode( $request['body'] );
         echo "<pre>"; var_dump($token); echo "</pre>";
+    }
+
+    public function accessToken()
+    {
+        $token = get_transient( 'twitter_access_token' );
+        $token = ( empty( $token ) ) ? get_twitter_access_token() : $token;
+        $request = wp_remote_get('https://api.twitter.com/1.1/followers/ids.json?screen_name=danielpataki&count=5', array(
+            'headers' => array(
+                'Authorization' => 'Bearer ' . $token,
+                'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8'
+            ),
+            'httpversion' => '1.1'
+        ));
+        $token = json_decode( $request['body'] );
     }
 }
