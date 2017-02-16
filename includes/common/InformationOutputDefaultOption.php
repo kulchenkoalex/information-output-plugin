b79df54097219edf6ef46ced37077ba59e029903
kulchenkoalex <alexej07@bk.ru>
2017-02-16 11:23:34 +0200

    Added method returns an array of default settings

--- includes/common/InformationOutputDefaultOption.php
+++ includes/common/InformationOutputDefaultOption.php
@@ -11,5 +11,15 @@ namespace includes\common;
 
 class InformationOutputDefaultOption
 {
-
+    public static function getDefaultOptions()
+    {
+        $defaults = array(
+            'account' => array(
+                'marker' => '',
+                'token' => ''
+            )
+        );
+        $defaults = apply_filters('information_output_default_option', $defaults);
+        return $defaults;
+    }
 }
