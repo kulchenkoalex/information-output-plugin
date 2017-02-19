7a36977263c1a0573abffc9d5efe43da4b0eb48e
kulchenkoalex <alexej07@bk.ru>
2017-02-19 23:06:32 +0200

    Contact information class model

---
+++ includes/models/admin/menu/InformationOutputContactInformationSubMenuModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 19.02.2017
+ * Time: 23:06
+ */
+
+namespace includes\models\admin\menu;
+
+
+class InformationOutputContactInformationSubMenuModel
+{
+
+}
