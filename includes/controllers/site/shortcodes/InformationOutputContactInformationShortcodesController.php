99b44d514b696950b933aa56de70da61d663371a
kulchenkoalex <alexej07@bk.ru>
2017-02-22 22:35:35 +0200

    Connect ajax handler

--- includes/controllers/site/shortcodes/InformationOutputContactInformationShortcodesController.php
+++ includes/controllers/site/shortcodes/InformationOutputContactInformationShortcodesController.php
@@ -13,6 +13,25 @@ use includes\controllers\admin\menu\InformationOutputICreatorInstance;
 class InformationOutputContactInformationShortcodesController extends InformationOutputShortcodesController
      implements InformationOutputICreatorInstance
 {
+    // Переопределим конструктор
+    public function __construct(){
+        // Вызов конструктора класса InformationOutputShortcodesController
+        // Чтобы прикрепить функцию к действию add_action( 'wp_loaded',  array( &$this, 'initShortcode') );
+        parent::__construct();
+
+        // подключаем AJAX обработчики, только когда в этом есть смысл
+        if( defined('DOING_AJAX') && DOING_AJAX ){
+            add_action('wp_ajax_contact_information', array( &$this, 'ajaxHandler'));
+            add_action('wp_ajax_nopriv_contact_information',  array( &$this, 'ajaxHandler'));
+        }
+        // Переменная ajaxurl
+     }
+    /**
+     * Обработчик для ajax действия guest_book (wp_ajax_guest_book, wp_ajax_nopriv_guest_book)
+     */
+    public function ajaxHandler(){
+
+        }
 
     public static function newInstance()
     {
