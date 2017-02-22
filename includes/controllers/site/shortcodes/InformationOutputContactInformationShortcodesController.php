2be2a0a13bca4643d667bcbe1dca3f40e6713b0a
kulchenkoalex <alexej07@bk.ru>
2017-02-22 22:01:59 +0200

    Adding shortcode 'information_output_contact_information'

---
+++ includes/controllers/site/shortcodes/InformationOutputContactInformationShortcodesController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Алексей
+ * Date: 22.02.2017
+ * Time: 19:46
+ */
+
+namespace includes\controllers\site\shortcodes;
+
+use includes\controllers\admin\menu\InformationOutputICreatorInstance;
+
+class InformationOutputContactInformationShortcodesController extends InformationOutputShortcodesController
+     implements InformationOutputICreatorInstance
+{
+
+    public static function newInstance()
+    {
+        // TODO: Implement newInstance() method.
+    }
+
+    /**
+     * Функция в которой будем добалять шорткоды через функцию add_shortcode( $tag , $func );
+     * @return mixed
+     */
+    public function initShortcode()
+    {
+        // TODO: Implement initShortcode() method.
+        //этот шорткод будет добалять форму для добавления данных
+         add_shortcode( 'information_output_contact_information', array(&$this, 'action'));
+    }
+
+    /**
+     * Функция обработки шоткода
+     * Функция указанная в параметре $func, получает 3 параметра, каждый из них может быть передан,
+     * а может нет:
+     * $atts(массив)
+     *      Ассоциативный массив атрибутов указанных в шоткоде. По умолчанию пустая строка - атрибуты
+     *      не переданы.
+     *      По умолчанию: ''
+     * $content(строка)
+     *      Текст шоткода, когда используется закрывающая конструкция шотркода: [foo]текст шорткода[/foo]
+     *      По умолчанию: ''
+     * $tag(строка)
+     *      Тег шорткода. Может пригодится для передачи в доп. функции. Пр: если шорткод - [foo],
+     *      то тег будет - foo.
+     *      По умолчанию: текущий тег
+     * @param array $atts
+     * @param string $content
+     * @param string $tag
+     * @return mixed
+     */
+    public function action($atts = array(), $content = '', $tag = '')
+    {
+        // TODO: Implement action() method.
+        return $this->render(array());
+    }
+
+    /**
+     * Функция отвечающа за вывод обработаной информации шорткодом
+     * @param $data
+     * @return mixed
+     */
+    public function render($data)
+    {
+        // TODO: Implement render() method.
+    }
+}
