c7dca59d538bdbc33ef9598596768579e0d22d85
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-02-23 11:57:34 +0200

    Creating a class to display the widget on the console

---
+++ includes/widgets/InformationOutputContactInformationDashboardWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Сервис
+ * Date: 23.02.2017
+ * Time: 11:56
+ */
+
+namespace includes\widgets;
+
+
+class InformationOutputContactInformationDashboardWidget
+{
+
+}
