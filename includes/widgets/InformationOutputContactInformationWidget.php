e37ade4336b3142443f27c27246021eceffb4f40
kulchenkoalex <kulchenko.alexey@gmail.com>
2017-02-23 11:53:34 +0200

    Method widget()

--- includes/widgets/InformationOutputContactInformationWidget.php
+++ includes/widgets/InformationOutputContactInformationWidget.php
@@ -9,6 +9,8 @@
 
 namespace includes\widgets;
 
+use includes\models\admin\menu\InformationOutputContactInformationSubMenuModel;
+
 class InformationOutputContactInformationWidget extends \WP_Widget
 {
     public function __construct() {
@@ -59,4 +61,56 @@ class InformationOutputContactInformationWidget extends \WP_Widget
         $values["text"] = htmlentities($newInstance["text"]);
         return $values;
      }
+
+    public function widget($args, $instance) {
+        $title = $instance["title"];
+        $text = $instance["text"];
+        echo "<h2>$title</h2>";
+        echo "<p>$text</p>";
+        // Вывод таблички гостевой книги
+        $data = InformationOutputContactInformationSubMenuModel::getAll();
+        ?>
+        <table  border="1">
+            <thead>
+            <tr>
+                <td>
+                    <?php _e('Имя пользователя', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN ); ?>
+                </td>
+                <td>
+                    <?php _e('Фамилия', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN ); ?>
+                </td>
+                <td>
+                    <?php _e('Номер телефона', INFORMATIONOUTPUT_PlUGIN_TEXTDOMAIN ); ?>
+                </td>
+
+            </tr>
+            </thead>
+            <tbody>
+            <?php if(count($data) > 0 && $data !== false){  ?>
+                <?php foreach($data as $value): ?>
+                    <tr class="row table_box">
+
+                        <td>
+                            <?php echo $value['user_name']; ?>
+                        </td>
+                        <td>
+                        <?php echo $value['user_surname']; ?>
+                        </td>
+                        <td>
+                        <?php echo $value['phone_number']; ?>
+                        </td>
+
+                    </tr>
+                <?php endforeach ?>
+            <?php }else{ ?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+        <?php
+    }
 }
