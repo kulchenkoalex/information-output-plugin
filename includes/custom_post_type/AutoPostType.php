82070edcf88a349ffc305e55c0342cc57cbf0766
kulchenkoalex <alexej07@bk.ru>
2017-02-26 21:43:35 +0200

    Custom Post Type Auto: Messages when you publish or change the type of recording and registration of two taxonomies marki and models for the blog-type auto

--- includes/custom_post_type/AutoPostType.php
+++ includes/custom_post_type/AutoPostType.php
@@ -16,13 +16,18 @@ class AutoPostType
         * Регистрируем Custom Post Type
         */
         add_action( 'init', array( &$this, 'registerAutoPostType' ) );
+        // хук, через который подключается функция
+        // регистрирующая новые таксономии  createAutoTaxonomies
+        add_action( 'init', array( &$this, 'createAutoTaxonomies' ) );
         // Сообщения при публикации или изменении типа записи auto
         add_filter('post_updated_messages',  array( &$this, 'autoUpdatedMessages' ));
+        // Раздел "помощь" типа записи auto
+        add_action( 'contextual_help', array( &$this, 'addHelpText' ), 10, 3 );
     }
 
-    function register_post_types()
+    function registerAutoPostType()
     {
-        register_post_type('post_type_name', array(
+        register_post_type('auto', array(
             'label' => null,
             'labels' => array(
                 'name' => 'Автомобили', // основное название для типа записи
@@ -71,5 +76,81 @@ class AutoPostType
             10 => sprintf( 'Черновик записи Auto обновлен. <a target="_blank" href="%s">Предпросмотр записи auto</a>', esc_url( add_query_arg( 'preview', 'true', get_permalink($post->ID) ) ) ),
         );
         return $messages;
+    }
+
+    public function addHelpText($contextual_help, $screen_id, $screen ){
+        //$contextual_help .= print_r($screen); // используйте чтобы помочь определить параметр $screen->id
+        if('book' == $screen->id ) {
+            $contextual_help = '
+		<p>Напоминалка при редактировании записи auto:</p>
+		<ul>
+			<li>Указать марку автомобиля, например, "Audi", "BMW".</li>
+			<li>Указать модель автомобиля.</li>
+		</ul>
+		<p>Если нужно запланировать публикацию на будущее:</p>
+		<ul>
+			<li>В блоке с кнопкой "опубликовать" нажмите редактировать дату.</li>
+			<li>Измените дату на нужную, будущую и подтвердите изменения кнопкой ниже "ОК".</li>
+		</ul>
+		<p><strong>За дополнительной информацией обращайтесь:</strong></p>
+		<p><a href="/" target="_blank">Блог о WordPress</a></p>
+		<p><a href="http://wordpress.org/support/" target="_blank">Форум поддержки</a></p>
+		';
+        }
+        elseif( 'edit-book' == $screen->id ) {
+            $contextual_help = '<p>Этот раздел помощи показанный для типа записи Auto ...</p>';
+        }
+        return $contextual_help;
+    }
+
+    public function createAutoTaxonomies(){
+        // определяем заголовки для 'марка'
+        $labels = array(
+            'name' => _x( 'Марки', 'taxonomy general name' ),
+            'singular_name' => _x( 'Марка', 'taxonomy singular name' ),
+            'search_items' =>  __( 'Поиск марки' ),
+            'all_items' => __( 'Все марки' ),
+            'parent_item' => __( 'Родитель марка' ),
+            'parent_item_colon' => __( 'Родитель марка:' ),
+            'edit_item' => __( 'Изменить марку' ),
+            'update_item' => __( 'Обновить марку' ),
+            'add_new_item' => __( 'Добавить новую марку' ),
+            'new_item_name' => __( 'Название новой марки' ),
+            'menu_name' => __( 'Марка' ),
+        );
+        // Добавляем древовидную таксономию 'марка' (как категории) жанр
+        register_taxonomy('марка', array('auto'), array(
+            'hierarchical' => true,
+            'labels' => $labels,
+            'show_ui' => true,
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'марка' ),
+        ));
+        // определяем заголовки для 'writer'
+        $labels = array(
+            'name' => _x( 'Модели', 'taxonomy general name' ),
+            'singular_name' => _x( 'Модель', 'taxonomy singular name' ),
+            'search_items' =>  __( 'Найти модель' ),
+            'popular_items' => __( 'Популярные модели' ),
+            'all_items' => __( 'Все модели' ),
+            'parent_item' => null,
+            'parent_item_colon' => null,
+            'edit_item' => __( 'Редактировать модель' ),
+            'update_item' => __( 'Обновить модель' ),
+            'add_new_item' => __( 'Добавить новую модель' ),
+            'new_item_name' => __( 'Новое имя модели' ),
+            'separate_items_with_commas' => __( 'Отдельные модели' ),
+            'add_or_remove_items' => __( 'Добавить или удалить модели' ),
+            'choose_from_most_used' => __( 'Выберите одну из наиболее используемых моделей' ),
+            'menu_name' => __( 'Модели' ),
+        );
+        // Добавляем НЕ древовидную таксономию 'writer' (как метки)
+        register_taxonomy('модель', 'auto',array(
+            'hierarchical' => false,
+            'labels' => $labels,
+            'show_ui' => true,
+            'query_var' => true,
+            'rewrite' => array( 'slug' => 'модель' ),
+        ));
     }
 }
